3767e49f66d1f365e41caa29c4a79be7bf78d938
wangfan <tianlianyouren@163.com>
2018-11-18 17:22:57 +0800

    服务端：动态设置服务启动的端口号

--- Rpc_Server/src/main/java/com/sky/Rpc_Server/App.java
+++ Rpc_Server/src/main/java/com/sky/Rpc_Server/App.java
@@ -1,5 +1,8 @@
 package com.sky.Rpc_Server;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,7 +15,21 @@ import com.rpc.rsf.provide.ProvideServer;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	
+    	Map<String, String> argMap=new HashMap<String, String>();
+    	for(String str:args) {
+    		if(str.startsWith("-")) {
+    			String[] kv = str.replaceAll("-", "").split("=");
+    			argMap.put(kv[0], kv[1]);
+    		}
+    	}
+    	String def_port="9008";
+//    	if(!StringUtils.isEmpty(argMap.get("port"))) {
+//    		def_port=argMap.get("port");
+//    	}
+    	System.setProperty("rpc.provider.server.port", def_port);
+    	String port = System.getProperty("rpc.provider.server.port");
+    	System.err.println("rpc port def:"+port);
     	ApplicationContext application =new ClassPathXmlApplicationContext("classpath:application.xml");
         ProvideServer provideServer = application.getBean(ProvideServer.class);
         provideServer.start();
