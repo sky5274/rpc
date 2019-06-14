9ccce712688e0d4c052963bd1233c015a3135d8f
wangfan <tianlianyouren@163.com>
2019-06-14 22:38:16 +0800

    spring  regist rpc consumer bean modify

--- rsf/src/main/java/com/rpc/rsf/spring/RpcConsumerDefinitionParser.java
+++ rsf/src/main/java/com/rpc/rsf/spring/RpcConsumerDefinitionParser.java
@@ -1,8 +1,6 @@
 package com.rpc.rsf.spring;
 
 
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -11,9 +9,7 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.w3c.dom.Element;
-
-import com.rpc.rsf.base.RpcConfig;
-import com.rpc.rsf.base.RpcConfig.nodeData;
+import com.alibaba.fastjson.JSON;
 import com.rpc.rsf.base.RpcElement;
 import com.rpc.rsf.consumer.ProxyFactory;
 
@@ -31,22 +27,22 @@ public class RpcConsumerDefinitionParser implements BeanDefinitionParser  {
 
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
 		RpcElement rpcEle = getRpcElement(element);
-		logger.info("rpt client config regist:" +rpcEle.writeUrl());
+		logger.info("rpt consumer config regist bean:" +JSON.toJSONString(rpcEle));
 		Class<?> clazz;
 		try {
 			clazz = Class.forName(rpcEle.getClassName());
-			List<String> serverlist = RpcConfig.getInitServerUrl(rpcEle.writeUrl());
-			if(serverlist.isEmpty()) {
-				throw new Exception("no server can consumer:"+rpcEle.getClassName());
-			}
-			BeanDefinition bean=null;
-			for(String server:serverlist) {
-				nodeData node = RpcConfig.getRandomServer(server);
-				if(node!=null) {
-					bean=registBean(node.getClassName().substring(node.getClassName().lastIndexOf("."), node.getClassName().length()),clazz,node.getClassName(),server,parserContext);
-				}
-			}
-			return bean;
+//			List<String> serverlist = RpcConfig.getInitServerUrl(rpcEle.writeUrl());
+//			if(serverlist.isEmpty()) {
+//				throw new Exception("no server can consumer:"+rpcEle.getClassName());
+//			}
+//			BeanDefinition bean=null;
+//			for(String server:serverlist) {
+//				nodeData node = RpcConfig.getRandomServer(server);
+//				if(node!=null) {
+//					bean=registBean(node.getClassName().substring(node.getClassName().lastIndexOf("."), node.getClassName().length()),clazz,node.getClassName(),server,parserContext);
+//				}
+//			}
+			return registBean(rpcEle.getClassName().substring(rpcEle.getClassName().lastIndexOf(".")+1, rpcEle.getClassName().length()),clazz,rpcEle,parserContext);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
@@ -65,13 +61,12 @@ public class RpcConsumerDefinitionParser implements BeanDefinitionParser  {
 	* @param parserContext
 	* @return
 	 */
-	private GenericBeanDefinition registBean(String id, Class<?> intertface,String interfaceImpl,String mapperUrl, ParserContext parserContext) {
+	private GenericBeanDefinition registBean(String id, Class<?> intertface,RpcElement node, ParserContext parserContext) {
 		logger.info("rpt load interface id :"+id+">> class :"+intertface.getName());
 		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(intertface);
 		GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
 		definition.getPropertyValues().add("interfaceClass", definition.getBeanClassName());
-		definition.getPropertyValues().add("interfaceImpl", interfaceImpl);
-		definition.getPropertyValues().add("mapperUrl", mapperUrl);
+		definition.getPropertyValues().add("node", node);
 		definition.setBeanClass(ProxyFactory.class);
 		definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
 		parserContext.getRegistry().registerBeanDefinition(id,definition);
@@ -83,6 +78,7 @@ public class RpcConsumerDefinitionParser implements BeanDefinitionParser  {
 		rpcEle.setType("provider");
 		rpcEle.setId(element.getAttribute("id"));
 		rpcEle.setClassName(element.getAttribute("class"));
+		rpcEle.setTarget(element.getAttribute("target"));
 		rpcEle.setGroup(element.getAttribute("group"));
 		rpcEle.setVersion(element.getAttribute("version"));
 		return rpcEle;
