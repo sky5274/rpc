c387cee7207ae5c7eb100e6deb30421d71233785
wangfan <tianlianyouren@163.com>
2019-06-14 22:22:56 +0800

    自定义rpc异常

---
+++ rsf/src/main/java/com/rpc/rsf/base/RpcException.java
@@ -0,0 +1,13 @@
+package com.rpc.rsf.base;
+
+public class RpcException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public RpcException(String string) {
+		super(string);
+	}
+
+}
