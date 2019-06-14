6c7dba329edd8867411955931dab2d2830fc48fe
wangfan <tianlianyouren@163.com>
2019-06-14 22:21:38 +0800

    rpc服务客户端

--- rsf/src/main/java/com/rpc/rsf/base/client/RpcClient.java
+++ rsf/src/main/java/com/rpc/rsf/base/client/RpcClient.java
@@ -1,51 +1,34 @@
-package com.rpc.rsf.consumer;
+package com.rpc.rsf.base.client;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
-import java.net.Socket;
-import java.net.SocketAddress;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import com.rpc.rsf.base.Result;
+import java.net.InetSocketAddress;
+import com.rpc.rsf.base.RpcConfig;
 import com.rpc.rsf.base.RpcRequest;
+import com.rpc.rsf.base.netty.client.RpcNettyClientHandel;
+import com.rpc.rsf.base.socket.client.RpcSocketClientHandel;
 
 public class RpcClient <T>{
-	private Log logger=LogFactory.getLog(getClass());
-	private SocketAddress addr;
-	public RpcClient(SocketAddress addr){
+	
+	private InetSocketAddress addr;
+	public RpcClient(InetSocketAddress addr){
 		this.addr=addr;
 	}
 	
-	public T request(Object proxy, Method method, Object[] args, Class<T> serviceInterface,String interfaceImpl) throws Exception {
-		logger.debug("rpt client proxt mentod: "+serviceInterface.getName()+"."+method.getName());
-        Socket socket = null;
-        ObjectOutputStream output = null;
-        ObjectInputStream input = null;
-        try {
-            // 2.创建Socket客户端，根据指定地址连接远程服务提供者
-            socket = new Socket();
-            
-			socket.connect(addr);
-			logger.debug("rpt client connect server:"+addr);
-            // 3.将远程服务调用所需的接口类、方法名、参数列表等编码后发送给服务提供者
-            output = new ObjectOutputStream(socket.getOutputStream());
-            RpcRequest request=new RpcRequest(interfaceImpl==null?serviceInterface.getName():interfaceImpl,method.getName(),method.getParameterTypes(),args);
-            output.writeObject(request);
-            logger.debug("rpt client send msg:"+request);
-            // 4.同步阻塞等待服务器返回应答，获取应答后返回
-            input = new ObjectInputStream(socket.getInputStream());
-             @SuppressWarnings("unchecked")
-			Result<T> result=(Result<T>) input.readObject();
-             logger.debug("rpt client get reuslt: "+result);
-             if(result.hasException()) {
-            	 throw result.getException();
-             }
-            return result.getData();
-        } finally {
-            if (socket != null) socket.close();
-            if (output != null) output.close();
-            if (input != null) input.close();
-        }
+	public T request(Class<T> serviceInterface, Method method, Object[] args,String interfaceImpl) throws Throwable {
+		 RpcRequest request=new RpcRequest(interfaceImpl==null?serviceInterface.getClass().getName():interfaceImpl,method.getName(),method.getParameterTypes(),args);
+		 RpcClientHandel rpcClientHandel;
+		 if(RpcConfig.isSocketServer()) {
+			 rpcClientHandel=new RpcSocketClientHandel();
+		 }else {
+			 rpcClientHandel=new RpcNettyClientHandel();
+		 }
+		 try {
+			 return  rpcClientHandel.invoke(request,addr);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			throw e;
+		}
+         
+        
 	}
 }
---
+++ rsf/src/main/java/com/rpc/rsf/base/client/RpcClientHandel.java
@@ -0,0 +1,9 @@
+package com.rpc.rsf.base.client;
+
+import java.net.InetSocketAddress;
+import com.rpc.rsf.base.RpcRequest;
+
+public interface RpcClientHandel {
+	public <T> T invoke(RpcRequest request, InetSocketAddress addr) throws Throwable;
+	
+}
