f2fdbf2f8c180a561a7c08504a5899cf6f08f912
wangfan <tianlianyouren@163.com>
2019-06-15 17:38:03 +0800

    rpc  客户端多线程测试

--- Rpc_Client/src/main/java/com/sky/Rpc_Client/App.java
+++ Rpc_Client/src/main/java/com/sky/Rpc_Client/App.java
@@ -22,7 +22,7 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException
     {
-    	ApplicationContext application =new ClassPathXmlApplicationContext("classpath:application.xml");
+    	final ClassPathXmlApplicationContext application =new ClassPathXmlApplicationContext("classpath:application.xml");
         System.out.println( "Hello World!" );
         //java proxy get interface
 //        UserService userserver = RpcProxy.getRemoteProxyObj(UserService.class, new InetSocketAddress("127.0.0.1", 9000));
@@ -32,12 +32,28 @@ public class App
 //		} catch (OneException e) {
 //				e.printStackTrace();
 //		}
-       
-        //spring get interface
+        active(application,"tome");
+        Thread.sleep(500);
+        for(int i=0;i<50;i++) {
+        	final int index=i;
+        	new Thread(new Runnable() {
+				public void run() {
+					active(application, "demo_"+index);
+					
+				}
+			}).start();
+        	Thread.sleep(500);
+        }
+        Thread.sleep(60*1000);
+        application.close();
+    }
+    
+    private static void active(ApplicationContext application,String param) {
+    	   //spring get interface
         try {
-        	 UserService server=(UserService) application.getBean(UserService.class);
-			System.out.println(server.getUserInfo("tome"));
-			System.err.println(server.testSevice(new testService()));
+        	UserService server=(UserService) application.getBean(UserService.class);
+			System.out.println(param+" >>> server.getUserInfo >>>"+server.getUserInfo(param));
+			System.err.println(param+" >>> server.testSevice >>>"+server.testSevice(new testService(param)));
 //			server.getUserAddr("sdfsd");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -50,11 +66,15 @@ class testService implements  RpcCallBack,TestService{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String param;
+	public testService(String param) {
+		this.param=param;
+	}
 
 	public String test(String some) {
 		System.out.println("test service");
-		UserBean user=new UserBean("tome", some);
+		UserBean user=new UserBean("tome_"+param, some);
 		user.say();
-		return user.getWord();
+		return user.getWord()+param;
 	}
 }
