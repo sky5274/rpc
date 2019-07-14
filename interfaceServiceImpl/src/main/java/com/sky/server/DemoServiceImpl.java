35a224f3992ec4350a2e8852dbdc65d7c226f094
wangfan <tianlianyouren@163.com>
2019-07-14 20:22:24 +0800

    rpc service impl modify

--- interfaceServiceImpl/src/main/java/com/sky/server/DemoServiceImpl.java
+++ interfaceServiceImpl/src/main/java/com/sky/server/DemoServiceImpl.java
@@ -1,7 +1,7 @@
 package com.sky.server;
 
 
-import com.rpc.rsf.base.RpcProvider;
+import com.rpc.rsf.annotation.RpcProvider;
 import com.sky.service.TestService;
 
 @RpcProvider(clazz=TestService.class,group="test",version="1-2")
